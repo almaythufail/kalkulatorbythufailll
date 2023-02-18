5fb2843cb6df5ceb4f69e5c97ec50876284b722c
almaythufail <125484087+almaythufail@users.noreply.github.com>
2023-02-18 16:57:54 +0700

    Update Aplikasi Kalkulator Volume Kerucut.c

--- Aplikasi Kalkulator Volume Kerucut.c
+++ Aplikasi Kalkulator Volume Kerucut.c
@@ -1,5 +1,5 @@
-/* Aplikasi Penghitung Volume Kerucut by Al May Nabith Thufail
-NRP 5010221007 
+/* Aplikasi Kalkulator Volume Kerucut by Al May Nabith Thufail
+NRP 5010221007
 Departemen Teknik Sistem dan Industri
 Institut Teknologi Sepuluh Nopember
 Algoritma dan Pemrograman Komputer (D)*/
@@ -9,23 +9,21 @@ Algoritma dan Pemrograman Komputer (D)*/
 int main () {
     
     float jari_jari,tinggi;
-    float phi = 3.14;
     
     printf ("Selamat Datang di Program Kalkulator Volume Kerucut by Al May Nabith Thufail\n");
-    printf ("Pengguna Dapat Memasukkan Nilai Jari-Jari dan Tinggi dari Kerucut\n\n");
+    printf ("Pengguna Dapat Memasukkan Nilai Jari-Jari dan Tinggi Sebagai Volume dari Kerucut\n\n");
     
     printf ("Masukkan Jari-Jari Sebagai Jari-Jari Kerucut disini: ");
     scanf ("%f", &jari_jari);
     printf ("Kamu Telah Memasukkan %.3f Sebagai Jari-Jari Kerucut\n\n", jari_jari);
     
-    printf ("Masukkan Jari-Jari Sebagai Tinggi Kerucut disini: ");
+    printf ("Masukkan Tinggi sebagai Tinggi Kerucut disini: ");
     scanf ("%f", &tinggi);
     printf ("Kamu Telah Memasukkan %.3f Sebagai Tinggi Kerucut\n\n", tinggi);
     
-    float hasil = ((phi*(jari_jari*jari_jari)*tinggi)/3);
-
-    printf ("Hasil dari Perhitungan Volume Kerucut Setelah Input Jari-Jari dan Tinggi Kerucut Adalah:  %.3f\n\n", hasil);
-    
-    printf ("Terima Kasih Sudah Menggunakan Program Kalkulator Volume Kerucut by Al May Nabith Thufail");
+    float phi = (3.14);
+    float hasil = ((phi*jari_jari*jari_jari*tinggi)/3);
+    printf ("Hasil dari Perhitungan Volume Kerucut Setelah Input Jari-Jari dan Tinggi Adalah: %.3f", hasil);
+   
     return 0;
 }
